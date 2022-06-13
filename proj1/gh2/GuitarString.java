26a1529f7ed4eeb7f2e9d3ec49072c8f094c2d5d
enosyu <enos.yu@outlook.com>
2022-06-13 17:24:43 +0800

    style checker

--- proj1/gh2/GuitarString.java
+++ proj1/gh2/GuitarString.java
@@ -20,7 +20,7 @@ public class GuitarString {
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
         buffer = new LinkedListDeque<>();
-        int capacity = (int) Math.round(SR/frequency);
+        int capacity = (int) Math.round(SR / frequency);
         for (int i = 0; i < capacity; i++) {
             buffer.addLast(0.0);
         }
@@ -34,7 +34,7 @@ public class GuitarString {
         //       other. This does not mean that you need to check that the numbers
         //       are different from each other. It means you should repeatedly call
         //       Math.random() - 0.5 to generate new random numbers for each array index.
-        for (int i = 0; i < buffer.size(); i++){
+        for (int i = 0; i < buffer.size(); i++) {
             buffer.removeFirst();
             double r = Math.random() - 0.5;
             buffer.addLast(r);
